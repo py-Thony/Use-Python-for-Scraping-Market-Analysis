597f4c7f50aa0e60fcf0fcdf6648932f91f6e18d
py-thony <57870398+py-Thony@users.noreply.github.com>
2021-06-14 19:23:41 +0200

    Beta version (ask to be translated in english)

--- scrapBooking.py
+++ scrapBooking.py
@@ -1,34 +1,56 @@
 # -*- coding: utf-8 -*-
 
 """
-script de scraping pour récupérer 1000 livres
-sous forme de fichiers CSV individuels par catégories.
+Ce script se charge d'interroger le site www.books.toscrape.com
 
-Au passage, seront récupérées les images des livres
-dans un dossier séparé.
+CONCEPTION:
+Etape 1: Extraction des URLS de chaque catégorie et mise en liste.
+
+Etape 2: Epluchage de cette liste pour scraper tous les liens de livre
+        et mise en liste générale de tuples (nom catégorie, [liste liens])
+
+Etape 3: Interrogation de chaque lien de livre pour:
+                    - Extraction des informations textuelles
+                    - Téléchargement de l'image du livre
+Etape 3bis: Création de dossiers parents auto-nommés selon le nom de la catégorie,
+            Création de dossiers enfants 'CSV' et 'IMAGES'
+            Enregistrement des données textuelles dans un fichier CSV dans le dossier CSV
+            Enregistrement des images renommées avec le titre du livre dans le dossier IMAGES
+
+Rencontrant des problèmes de contrôle de l'endroit où créer les dossiers,
+(sous machine Windows10, VScode) il à été nécessaire de créer une fonction
+'CreateFolder' et de définir le Path manuellement.
+
+Rencontrant des cas particuliers dans les titres de livres, il à été necessaire
+de gérer ces exceptions en remplaçant certains symboles. (eg: virgules <!> CSV)
+
+ARBORESCENCE:
+scrapBooking.py >>> fonctionnement général du programme et paramétrages
+scrapBookingFuncions.py >>> routines répétitives passées sous forme de fonctions à appeler
+
+GENERATION DE DOSSIERS ET FICHIERS:
+dossier parent portant le nom de la catégorie
+    --> Dossier enfant nommé CSV
+        --> finchier.csv portant le nom de la catégorie
+    --> Dossier enfant nommé IMAGES
+        --> enregistrement des images au format JPG et renommées avec le titre du livre
+
 """
 
 ########################################
 # Importations des modules nécessaires #
 ########################################
-
-# Module d'interaction avec le système d'exploitation
 import os
-# Module de parsage HTML en langage humain
-from bs4 import BeautifulSoup as Soup
-# Module de gestion du temps pour espacer les requêtes
-import time
-# Module de gestion du format CSV
 import csv
 
 ##########################################
 # Importation des fonctions personnelles #
 ##########################################
 
-from scrapBookingFunctions import CreateFolder, scrapLinksOfCategories
-from scrapBookingFunctions import scrapLinksOfBooks
-from scrapBookingFunctions import scrapBookInformations
-from scrapBookingFunctions import scrapAndSaveBookImage
+from scrapBookingFunctions import create_folder
+from scrapBookingFunctions import scrap_links_of_categories
+from scrapBookingFunctions import scrap_links_of_books
+from scrapBookingFunctions import scrap_book_informations
 
 """ Dans un premier temps, il faut se connecter au site ciblé
 
@@ -44,28 +66,54 @@ baseUrlCategories = 'http://books.toscrape.com/'
 # To rebuild the relative links of books
 baseUrlBooks = 'http://books.toscrape.com/catalogue/'
 
-# Une fonction est crée pour récupérer les liens des catégories
-linksOfCategories = scrapLinksOfCategories(websiteUrl, baseUrlCategories)
-
-""" Maintenant que nous avons notre liste de liens de catégories
-nous pouvons itérer chacun pour récupérer les liens des livres.
-
-La collecte de données se fera plus tard, il ne s'agit ici que de récupérer
-les liens et de les lister selon leur catégorie respective
-
-Nous utiliserons donc une simple boucle for
-"""
+# Auto remplissage de la liste des liens de catégories
+linksOfCategories = scrap_links_of_categories(
+                                websiteUrl,
+                                baseUrlCategories)
 
 allBooksLinksList = []
-
 for oneCategory in linksOfCategories:
-    # Defining names and links separately
-    categoryName = oneCategory[0]
-    print(categoryName)
+    # Gestion du cas particuliers des catégories à plusieurs pages
+    provisionalLinksList = []
+
+    nextPage = 1
+    booksScraped = 0
+    categoryName = oneCategory[0].capitalize()
     categoryLink = oneCategory[1]
+    expectedBooks = oneCategory[2]
+    nbPagesToScan = oneCategory[3]
+
+    print(categoryName, "\n", categoryLink)
+    while nextPage <= nbPagesToScan:
+        
+        nbBooksToScan = expectedBooks - booksScraped
+
+        if nextPage == 2:
+            print(f"Page N°{nextPage}")
+            categoryLink = categoryLink.replace(
+                "index.html",
+                f"page-{nextPage}.html")
+
+        elif nextPage > 2:
+            print(f"Page N°{nextPage}")
+            categoryLink = categoryLink.replace(
+                f"page-{nextPage-1}.html",
+                f"page-{nextPage}.html")
+
+        scanPageInCourse = scrap_links_of_books(
+                                categoryLink,
+                                baseUrlBooks,
+                                nbBooksToScan)
+
+        for element in scanPageInCourse:
+            provisionalLinksList.append(element)
+
+        booksScraped += len(scanPageInCourse)
+        nextPage += 1
+    print(">>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<")
     # Retrieving all book links for the current category
-    allBooksLinksList.append(
-        (categoryName, scrapLinksOfBooks(categoryLink, baseUrlBooks)))
+    allBooksLinksList.append((categoryName, provisionalLinksList))
+
 
 """ A ce stade nous disposons d'une liste générale
 contenant une sous-liste par catégorie.
@@ -79,8 +127,10 @@ Chaque sous-liste se présente sous forme d'un tuple,
 # à la catégorie suivante
 RootDir = os.path.dirname(os.path.abspath(__file__))
 fields=[
-    'Book Title','UPC Code','Product Type', 'Price (ExclTax)', 
-    'Price (InclTax)', 'Taxes', 'Availability', 'Image Url']
+    'Book Title','UPC Code','Product Type', 
+    'Price (ExclTax)', 'Price (InclTax)', 'Taxes', 
+    'Availability', 'number_of_reviews', 'nb_of_stars',
+    'Image Url']
 countBooks = 0
 for linksOfBookInOneCategory in allBooksLinksList:
 
@@ -88,22 +138,17 @@ for linksOfBookInOneCategory in allBooksLinksList:
 
     CSVpath = f"{RootDir}\\SCRAPED_FILES\\{categoryName}\\CSV_FILES\\"
     imagePath = f"{RootDir}\\SCRAPED_FILES\\{categoryName}\\IMAGES\\"
-    CreateFolder(CSVpath)
-    CreateFolder(imagePath)
+    create_folder(CSVpath)
+    create_folder(imagePath)
 
-    listOfInfoForOneBook = []
     listOfallBooksInfo = []
     for bookLink in linksOfBookInOneCategory[1]:
         countBooks += 1
         print(countBooks)
         # Retrieving information from the book
-        imageUrl, currentBookInfo = scrapBookInformations(bookLink)
+        currentBookInfo = scrap_book_informations(bookLink, imagePath)
         listOfallBooksInfo.append(list(currentBookInfo))
 
-        #time.sleep(0.5)
-
-        currentBookImage = scrapAndSaveBookImage(
-            imagePath, imageUrl, currentBookInfo[0])
     with open(
         f"{CSVpath}{categoryName}.csv",
         'w',
